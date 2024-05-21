be7da80c1d322ce58cc91a6fec33f88d169af0c5
Ganeshrocks2022 <168628101+Ganeshrocks2022@users.noreply.github.com>
2024-05-21 14:29:05 +0530

    Add files via upload

---
+++ Push.js
@@ -0,0 +1,11 @@
+let food1s = document.getElementById('food1')
+let food2s = document.getElementById('food2')
+let foods = document.getElementById('food')
+
+food1s.addEventListener('click',()=>{
+    foods.style.backgroundImages="url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOKSrHzWni6BYgvaGjTv2u3icp0XSaXGDXhyt2t3Dh-Cl3TINM6hnvD2FZ5LZe_JhNeMs&usqp=CAU')"
+})
+
+food2s.addEventListener('click',()=>{
+    foods.style.backgroundImages="url('https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/07/1296x728-header.jpg?w=1155&h=1528')"
+})
